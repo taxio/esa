e5161f7d57135aa4d2122c92bd39d5b668cf3677
taxio <taxio.2029@gmail.com>
2021-05-10 00:11:51 +0900

    refactor

--- root.go
+++ root.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"github.com/spf13/afero"
+	"os"
+
 	"github.com/spf13/cobra"
 	"github.com/srvc/fail/v4"
 	"github.com/taxio/esa/log"
-	"os"
 )
 
 func NewRootCmd() *cobra.Command {
-	var cfg *Config
 	cmd := &cobra.Command{
 		Use:     AppName,
 		Short:   "A cli tool for esa",
@@ -26,21 +25,17 @@ func NewRootCmd() *cobra.Command {
 				log.SetVerboseLogger(os.Stdout)
 				log.Println("verbose on")
 			}
-
-			// Load global config
-			c, err := LoadConfig(afero.NewOsFs())
-			if err != nil {
-				return fail.Wrap(err)
-			}
-			cfg = &c
 			return nil
 		},
 	}
 
 	cmd.PersistentFlags().Bool("verbose", false, "print log for developers")
 
-	//subCmds := []*cobra.Command{}
-	//cmd.AddCommand(subCmds...)
+	subCmds := []*cobra.Command{
+		NewConfigSubCmd(),
+		NewListSubCmd(),
+	}
+	cmd.AddCommand(subCmds...)
 
 	return cmd
 }
